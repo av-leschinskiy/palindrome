b0a01426160e0855f90fb83d55c0870834e6670a
leschinskiy <Leschinskiy.AV@dns-shop.ru>
2024-09-24 10:59:39 +1000

    переименовал class1 добавил метод проверки на палиндром

---
+++ Utility/PalindromeUtility.cs
@@ -0,0 +1,20 @@
+﻿namespace Utility
+{
+    public class PalindromeUtility
+    {
+        bool checkIsPalindrome(string input) {
+           
+            var length = input.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (input[i] != input[length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
